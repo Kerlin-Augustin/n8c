d1c37bd04e3bdc820641c654ba4b539fb20430b9
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-05-09 17:09:52 -0400

    pulling data from database

--- frontend/src/components/searchquery/SearchQuery.tsx
+++ frontend/src/components/searchquery/SearchQuery.tsx
@@ -1,15 +1,48 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
 const SearchQuery = () => {
 
-  // const style: { [key: string]: React.CSSProperties } = {
-  //   queryContainer: {
-  //     display: 'flex',
-  //     border: "1px solid red"
-  //   }
-  // }
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const style: { [key: string]: React.CSSProperties } = {
+    queryContainer: {
+      margin: "1em",
+    },
+    queryInput: {
+      width: '50em',
+      height: '2em',
+    }
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (searchQuery) {
+        axios
+          .get('/api/userCompany')
+          .then(res => {
+            console.log(searchQuery)
+            if (searchQuery.toLowerCase() === res.data[0].companyName.toLowerCase()) {
+              console.log(res.data[0]._id)
+            }
+          })
+      }
+    }, 300)
+    return () => clearTimeout(timeout)
+  }, [searchQuery])
+
+  const handleChange = (e: any) => {
+    setSearchQuery(e.target.value)
+  }
 
   return (
-    <div>
-      <input type='search' placeholder="Find Contracts"/>
+    <div style={style.queryContainer}>
+      <h3>Find Contracts</h3>
+      <input
+        type='search'
+        style={style.queryInput}
+        onChange={handleChange}
+      />
     </div>
   )
 }
